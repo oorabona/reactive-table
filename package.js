bbbd9bb6accf49135261a933d8194bfda9cc5761
Olivier ORABONA <olivier.orabona@gmail.com>
2015-12-07 16:53:36 +0100

    Added check and ejson as new deps. Version bump to 1.0.7.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'oorabona:reactive-table',
   summary: 'Reactive tables with many options',
-  version: '1.0.6',
+  version: '1.0.7',
   git: 'https://github.com/oorabona/reactive-table.git'
 });
 
@@ -12,9 +12,8 @@ Package.onUse(function(api) {
   api.use('coffeescript@1.0.1', 'client');
 
   // Core
-  api.use('templating', 'client');
-  api.use('jquery', 'client');
-  api.use('underscore', 'client');
+  var core = ["templating", "jquery", "check", "ejson", "underscore"]
+  api.use(core, 'client');
   api.use('reactive-var@1.0.3', 'client');
 
   api.addFiles([
